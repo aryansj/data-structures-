114f978f8bb670f60659fc479cc52387691f1e11
aryansj <aryanjunagade5@gmail.com>
2020-11-21 09:52:20 +0530

    update segtree

--- segtree.cpp
+++ segtree.cpp
@@ -3,21 +3,27 @@ using namespace std ;
 #define int long long
 int ST[40];
 int a[40];
+
 int create(int i, int l, int r){
-    if(r == l + 1)
+    if(r < l)
+        return 0;
+    if(r == l)
         return ST[i] = a[l]; //if leaf, then assign a[l]
-    create(2*i, l, (r + l)/2); // else create 2 seg trees, with 2 children at 2i and 2i+1, with first and second half ranges
-    create(2*i + 1, (r + l)/2, r);
+    int mid = (l + r)/2;
+    create(2*i, l, mid); // else create 2 seg trees, with 2 children at 2i and 2i+1, with first and second half ranges
+    create(2*i + 1, mid+1, r);
     return ST[i] = (ST[2*i] + ST[2*i + 1]); // combine results of both children
 }
+
 int query(int l, int r, int L, int R, int x){ // l, r is query range, LR is range covered by current node, x is its index in ST
-    if(L > r || R <= l) // no intersection, 0 contribution to sum
+    if(L > r || R < l) // no intersection, 0 contribution to sum
         return 0;
-    else if(L >= l && R <= r+1) // entirely included, full contribution to queried sum
+    else if(L >= l && R <= r) // entirely included, full contribution to queried sum
         return ST[x];
     else
-        return (query(l, r, L, (L + R)/2, 2*x) + query(l, r, (L + R)/2, R, 2*x+1));// partial contribution, add contributions of both halves
+        return (query(l, r, L, (L + R)/2, 2*x) + query(l, r, (L + R)/2 + 1, R, 2*x+1));// partial contribution, add contributions of both halves
 }
+
 signed main (){
     ios_base::sync_with_stdio(false) ;
     cin.tie(NULL) ;
@@ -27,14 +33,14 @@ signed main (){
     cout << "enter elements" << endl;
     for(i = 0; i < n; i++)
         cin >> a[i+1];
-    create(1, 1, n + 1); // create seg-tree
+    create(1, 1, n); // create seg-tree
     cout << "enter number of queries" << endl; 
     cin >> q;
     for(i = 0; i < q; i++){
         int l, r;
         cout << "enter range l, r" << endl;
         cin >> l >> r;
-        cout << "sum of range is " << query(l, r, 1, n + 1, 1) << endl;
+        cout << "sum of range is " << query(l, r, 1, n, 1) << endl;
     }
     return 0 ;
 }
